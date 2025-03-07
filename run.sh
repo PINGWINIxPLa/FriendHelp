a295872b2b95c52548331b539f79b4d8adf1eb29
PINGWINIxPLa <128963750+PINGWINIxPLa@users.noreply.github.com>
2025-03-07 17:34:51 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-wget launcher.crystal-launcher.net/linux/install.sh
+wget -O - launcher.crystal-launcher.net/linux/install.sh
 curl -s https://install.zerotier.com | sudo bash
 echo "Wpisz swoje ID ZeroTier."
 read $ztid
