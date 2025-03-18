df83b8e5e579936231956fc7121c500b80b870d4
PINGWINIxPLa <128963750+PINGWINIxPLa@users.noreply.github.com>
2025-03-18 16:32:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sudo apt install openssh-server -y
-wget -O - launcher.crystal-launcher.net/linux/install.sh
+wget -O - launcher.crystal-launcher.net/linux/install.sh | bash
 curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join fada62b0157fa866
